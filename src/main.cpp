39cb5e97d1cb4fa09dcb06e471af57e668213bf5
ppprisha <prisha.sujin@gmail.com>
2025-02-17 01:54:21 -0500

    finished 3

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,21 @@
 // std libraries
 #include <iostream>
 // our libraries
+#include "vec3.h"
+#include "color.h"
+
+// namespaces
+using namespace std;
 
 int main() {
 	int image_width = 256;
 	int image_height = 256;
 
 	// rendering
-	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+	cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 	for (int j = 0; j < image_height; j++) { // generates values for rows
+		clog << "\rscanlines remaining... " << (image_height - j) << ' ' << flush; // clog specifically for logging
 		for (int i = 0; i < image_width; i++) { // generates values columns
 			auto r = double(i) / (image_width - 1);
 			auto g = double(j) / (image_height - 1);
@@ -24,7 +30,8 @@ int main() {
 			int ig = int(255.999 * g);
 			int ib = int(255.999 * b);
 
-			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+			cout << ir << ' ' << ig << ' ' << ib << '\n';
 		}
 	}
+	clog << "\rscanlines remaining... 0!\n";
 }
